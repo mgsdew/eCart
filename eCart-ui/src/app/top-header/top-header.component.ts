4d282f125b8c2d4f8bbd8f0381f7431b99e87b17
mgsdew <mgsdew@gmail.com>
2024-05-20 08:51:00 +0600

    first commit

---
+++ eCart-ui/src/app/top-header/top-header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'top-header',
+  // selector: '[top-header]',
+  // selector: '.top-header',
+  // selector: '#top-header',
+  templateUrl: './top-header.component.html',
+  styleUrls: ['./top-header.component.css']
+})
+export class TopHeaderComponent {
+
+}
